821cf91a40189540a5d6d0a46079f2f8a83e8db2
Kenisha Johnson <kenishaajohnson@gmail.com>
2022-10-02 19:53:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+#Write your code below this row 👇
+even_numbers = 0 
+for even in range(2, 101, 2):
+    even_numbers += even
+print(even_numbers)
+
+#other way to solve: 
+# total2 = 0 
+# for number in range(1,101):
+#     if number % 2 == 0:
+#       total2 += number  
+
+# print(total2)
